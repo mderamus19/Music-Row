689649404291bfdf9f6c11af83865c14ae307927
Misty DeRamus <mistyderamus@gmail.com>
2019-06-19 15:42:18 -0500

    Initial set up

---
+++ main.js
@@ -0,0 +1,18 @@
+const recordLabels = [
+    {
+        label:"Jump Stop Records",
+        type: "Funk",
+        genre:"Rap"
+    },
+    {
+        label:"Chatten Records",
+        type: "Country",
+        genre:"Bluegrass"
+    },
+    {
+        label:"Polar Records",
+        type: "Pop",
+        genre:"Pop"
+    }
+]
+console.log(recordLabels)
